63d2e88a6d015e80c85a5bb67084e0d84c1f1244
Anu Heese <anuheese@gmail.com>
2024-09-20 15:51:20 +0100

    get and post

--- server/seed.js
+++ server/seed.js
@@ -1,1 +1,15 @@
-//here you can add your sql queries to create you table and dummy data 
+import { db } from "./server.js";
+
+db.query(`CREATE TABLE IF NOT EXISTS userComments (
+    id SERIAL PRIMARY KEY,
+    user_name VARCHAR(64),
+    comment VARCHAR(255)
+    )`);
+
+db.query(
+  `INSERT INTO userComments (user_name, comment) VALUES ('Sam', 'Top class hotel')`
+);
+
+db.query(
+  `INSERT INTO userComments (user_name, comment) VALUES ('Lisa', 'What a rubbish hotel!')`
+);
--- server/server.js
+++ server/server.js
@@ -1,9 +1,49 @@
-//you need two routes minimun
-//route to READ the database Data
-//route to CREATE and ADD new data to database
-//!in your create route you need the request.body is an object that from data coming from your client
-//you need to use SQL queries and parameters
+import express from "express";
+import cors from "cors";
 
-//in the .env you need your database connection string  with the correct password
+import dotenv from "dotenv";
+dotenv.config();
 
-//your .env file should contain your connection string from supabase
+import pg from "pg";
+const dbConnectionString = process.env.DATABASE_URL;
+export const db = new pg.Pool({
+  connectionString: dbConnectionString,
+});
+
+const app = express();
+app.use(express.json());
+
+app.use(cors());
+
+const PORT = 8081;
+app.listen(PORT, () => {
+  console.log(`server is running in PORT ${PORT}`);
+});
+
+app.get("/", (request, response) => {
+  response.json({ message: "You are looking at my root route" });
+});
+
+app.get("/userComments", async (req, res) => {
+  try {
+    const results = await db.query("SELECT * FROM userComments");
+    res.json(results.rows);
+  } catch (err) {
+    console.error("Error executing query", err);
+  }
+});
+
+app.post("/submittedUserComment", async function (request, response) {
+  const { user_name, comment } = request.body;
+  try {
+    await db.query(
+      `INSERT INTO userComments(user_name,comment) VALUES ($1, $2)`[
+        (user_name, comment)
+      ]
+    );
+    response.status(200).json({ success: true });
+  } catch (error) {
+    response.status(500).json({ success: false });
+    console.log("/submittedUserComment not found", error);
+  }
+});
