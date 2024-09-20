d9599a416dd4274f8962ba4302b6b0b62aa2f10a
Anu Heese <anuheese@gmail.com>
2024-09-20 15:52:13 +0100

    get and post

--- client/app.js
+++ client/app.js
@@ -1,3 +1,42 @@
+console.log("test");
+
+const userFeedback = document.getElementById("user-feedback");
+console.log(userFeedback);
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(userFeedback);
+  console.log(formData);
+  const formValues = Object.fromEntries(formData);
+  console.table(formValues);
+  const response = await fetch("http://localhost:8081/submittedUserComment", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ formValues }),
+  });
+  const data = await response.json();
+  console.log(data);
+}
+
+userFeedback.addEventListener("submit", handleSubmit);
+
+const userComments = document.getElementById("previous-user-comments");
+console.log(userComments);
+
+// async function getUserComments() {
+//   const response = await fetch(
+//     "https://cookie-upgrade-api.vercel.app/api/upgrades"
+//   );
+//   console.log(response);
+//   const shopData = await response.json();
+//   console.log(shopData);
+//   shopUpgrades = shopData;
+//   console.log(shopUpgrades);
+//   renderShopUpgrades();
+// }
+// function addUserComments() {}
 // DOM manipulation
 //select the form and user feedback
 
@@ -26,11 +65,11 @@
 //parse the response into json
 //wrangle data if necessary
 
-//I need to display the data on the page
-databaseData.forEach((item) => {
-  //I need to create DOM elements to contain the data
-  // !one DOM element (h1, h2, p, li) per piece of dat (username, comment..) --> if I am getting username and comment from the database, I need TWO DOM element, one for username and one for comments
-  //I ned to assign the values to the text content porperty
-  //for example, the text content property for my h1 will have a value of the username from my databse data
-  //I need to append thos elements to the DOM individualy
-});
+// //I need to display the data on the page
+// databaseData.forEach((item) => {
+//   //I need to create DOM elements to contain the data
+//   // !one DOM element (h1, h2, p, li) per piece of dat (username, comment..) --> if I am getting username and comment from the database, I need TWO DOM element, one for username and one for comments
+//   //I ned to assign the values to the text content porperty
+//   //for example, the text content property for my h1 will have a value of the username from my databse data
+//   //I need to append thos elements to the DOM individualy
+// });
